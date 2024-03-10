64ad1c4ddca188f5f847f0671f9ccd5d145705d2
awoliop <awoliioop@gmail.com>
2024-03-10 14:21:54 +0300

    reverted to the old ways and fixed

--- src/App.jsx
+++ src/App.jsx
@@ -9,39 +9,24 @@ import Footer from "./Footer";
 import { useState, useEffect } from "react";
 import "./App.css";
 import "./index.css";
-// import { Outlet, Router } from "react-router-dom";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/home",
-      element: <Home />,
-      errorElement: <Missing />,
-    },
-    {
-      path: "/posts",
-      element: <NewPost />,
-      errorElement: <Missing />,
-    },
-    {
-      path: "/postPage",
-      element: <PostPage />,
-      errorElement: <Missing />,
-    },
-    {
-      path: "/about",
-      element: <About />,
-      errorElement: <Missing />,
-    },
-  ]);
   return (
     <>
-      <div className="app_continer">
-        <Header />
-        <Nav />
-        <RouterProvider router={router} />
-        <Footer />
-      </div>
+      <Router>
+        <div className="app_continer">
+          <Header />
+          <Nav />
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/posts" element={<NewPost />} />
+            <Route path="/postPage" element={<PostPage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/*" element={<Missing />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
     </>
   );
 }
